84149e2ee01fb082a9c6326d06f8f0dcaa82e6ee
CrstKng <rmn.l.dimitrov@gmail.com>
2026-09-04 00:43:02 +0200

    reset and counter handlers added

--- handlers.go
+++ handlers.go
@@ -3,14 +3,24 @@ package main
 import (
 	"net/http"
 	"fmt"
+	"sync/atomic"
 )
 
-func handlerReadinessCheck(resWriter http.ResponseWriter, req *http.Request) {
-	resWriter.Header().Add("Content-Type", "text/plain; charset=utf-8")
-	resWriter.WriteHeader(200)
+func handlerReadinessCheck(w http.ResponseWriter, req *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
 	res := "OK"
-	_, err := resWriter.Write([]byte(res))
+	_, err := w.Write([]byte(res))
 	if err != nil {
 		fmt.Printf("error when writing data to body: %s\n", err)
 	}
+}
+
+func (cfg *apiConfig) handlerRequestCounter(w http.ResponseWriter, req *http.Request) {
+	w.Write([]byte(fmt.Sprintf("Hits: %d\n", cfg.fileserverHits.Load())))
+}
+
+func (cfg *apiConfig) handlerReset(w http.ResponseWriter, req *http.Request) {
+	var zeroValue atomic.Int32
+	cfg.fileserverHits = zeroValue
 }
--- main.go
+++ main.go
@@ -4,20 +4,37 @@ import (
 	"net/http"
 	"time"
 	"log"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
 
 func main() {
 	const filePathRoot = "."
 	const port = "8080"
+
+	var apiConfig apiConfig
+	apiCfg := &apiConfig
 
 	mux := http.NewServeMux()
 
 	filePath := http.Dir(filePathRoot)
 	handler := http.FileServer(filePath)
 	strippedFileServer := http.StripPrefix("/app", handler)
-	mux.Handle("/app/", strippedFileServer)
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(strippedFileServer))
 
 	mux.HandleFunc("/healthz", handlerReadinessCheck)
+	mux.HandleFunc("/metrics", apiCfg.handlerRequestCounter)
+	mux.HandleFunc("/reset", apiCfg.handlerReset)
 
 	srv := &http.Server{
 		Addr: ":" + port,
